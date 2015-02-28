cd6caff4c390c5558e7ae040cb6fa136a7ef7a4b
Troels Ynddal <troelsynddal@gmail.com>
2015-03-01 00:55:24 +0100

    Cleaned up read_png_file

--- Main.cpp
+++ Main.cpp
@@ -24,6 +24,7 @@
 #include <assert.h>
 
 #define PNG_DEBUG 3
+#define PNG_SIG_BYTES 8
 #include <png.h>
 
 using namespace std;
@@ -236,92 +237,43 @@ void abort_(const char * s, ...)
         abort();
 }
 
-//int x, y;
-
-
-
-#define PNG_SIG_BYTES 8
-
 GLuint read_png_file(const char * file_name, int * width, int * height){
     png_byte header[8];
 
     FILE *fp = fopen(file_name, "rb");
-    if (fp == 0)
-    {
-        perror(file_name);
-        //return 0;
-    }
+    assert(fp && "Could not open PNG file");
 
-    // read the header
     fread(header, 1, 8, fp);
+    assert(!png_sig_cmp(header, 0, PNG_SIG_BYTES) && "File is not a PNG");
 
-    if (png_sig_cmp(header, 0, 8))
-    {
-        fprintf(stderr, "error: %s is not a PNG.\n", file_name);
-        fclose(fp);
-        //return 0;
-    }
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL,
+                                                 NULL, NULL);
+    assert(png_ptr && "Could not create read struct for PNG");
 
-    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png_ptr)
-    {
-        fprintf(stderr, "error: png_create_read_struct returned 0.\n");
-        fclose(fp);
-        //return 0;
-    }
-
-    // create png info struct
     png_infop info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr)
-    {
-        fprintf(stderr, "error: png_create_info_struct returned 0.\n");
-        png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
-        fclose(fp);
-        //return 0;
-    }
+    assert(info_ptr && "Could not create info struct");
 
-    // create png info struct
     png_infop end_info = png_create_info_struct(png_ptr);
-    if (!end_info)
-    {
-        fprintf(stderr, "error: png_create_info_struct returned 0.\n");
-        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) NULL);
-        fclose(fp);
-        //return 0;
-    }
+    assert(end_info && "Could not create end infomation");
 
-    // the code in this if statement gets called if libpng encounters an error
-    if (setjmp(png_jmpbuf(png_ptr))) {
-        fprintf(stderr, "error from libpng\n");
-        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-        fclose(fp);
-        //return 0;
-    }
+    assert(!setjmp(png_jmpbuf(png_ptr)) && "libpng encountered an error");
 
-    // init png reading
     png_init_io(png_ptr, fp);
-
-    // let libpng know you already read the first 8 bytes
-    png_set_sig_bytes(png_ptr, 8);
-
-    // read all the info up to the image data
+    png_set_sig_bytes(png_ptr, PNG_SIG_BYTES);
     png_read_info(png_ptr, info_ptr);
 
-    // variables to pass to get info
-    int bit_depth, color_type;
-    png_uint_32 temp_width, temp_height;
-
-    // get info about png
-    png_get_IHDR(png_ptr, info_ptr, &temp_width, &temp_height, &bit_depth, &color_type,
-        NULL, NULL, NULL);
+    int bit_depth;
+    int color_type;
+    png_uint_32 temp_width;
+    png_uint_32 temp_height;
+    png_get_IHDR(png_ptr, info_ptr, &temp_width, &temp_height, &bit_depth,
+                 &color_type, NULL, NULL, NULL);
 
     if (width){ *width = temp_width; }
     if (height){ *height = temp_height; }
 
-    // Update the png info struct.
-    png_read_update_info(png_ptr, info_ptr);
 
-    // Row size in bytes.
+    png_read_update_info(png_ptr, info_ptr);
     int rowbytes = png_get_rowbytes(png_ptr, info_ptr);
 
     // glTexImage2d requires rows to be 4-byte aligned
@@ -330,29 +282,14 @@ GLuint read_png_file(const char * file_name, int * width, int * height){
     // Allocate the image_data as a big block, to be given to opengl
     png_byte * image_data;
     image_data = (png_byte *) malloc(rowbytes * temp_height * sizeof(png_byte)+15);
-    if (image_data == NULL)
-    {
-        fprintf(stderr, "error: could not allocate memory for PNG image data\n");
-        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-        fclose(fp);
-        //return 0;
-    }
+    assert(image_data != NULL && "Could not allocate memory for PNG image data");
 
     // row_pointers is for pointing to image_data for reading the png with libpng
     png_bytep * row_pointers = (png_bytep *)malloc(temp_height * sizeof(png_bytep));
-    if (row_pointers == NULL)
-    {
-        fprintf(stderr, "error: could not allocate memory for PNG row pointers\n");
-        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-        free(image_data);
-        fclose(fp);
-        //return 0;
-    }
+    assert(row_pointers != NULL && "Could not allocate memory for PNG row pointers");
 
     // set the individual row_pointers to point at the correct offsets of image_data
-    int i;
-    for (i = 0; i < temp_height; i++)
-    {
+    for (int i = 0; i < temp_height; i++){
         row_pointers[temp_height - 1 - i] = image_data + i * rowbytes;
     }
 
@@ -372,6 +309,7 @@ GLuint read_png_file(const char * file_name, int * width, int * height){
     free(image_data);
     free(row_pointers);
     fclose(fp);
+
     return texture;
 }
 
